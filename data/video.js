f0ed54c5026a7d38f567f30de74575386b3bb589
bryantjcoffey <bryant@totalmediagroup.com>
2014-03-15 14:26:34 -0700

    Main Reel

--- data/video.js
+++ data/video.js
@@ -1462,9 +1462,9 @@ var data = [
     video: [ [360,720], ["TMG_Reel_021214_SD","TMG_Reel_021214_HD"] ],
     client: "TMG",
     one_liner: "TMG Studio Reel 2014",
-    primary_text: [],
+    primary_text: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
     production_roles: ["creative","3D Animation","editorial"],
-    secondary_text: [""],
+    secondary_text: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
     secondary_content: [
       { title:"", image:"neil4", video:"", button_white_or_black:"white", title_white_or_black:"white" },
       { title:"", image:"", video:"", button_white_or_black:"white", title_white_or_black:"white" },
