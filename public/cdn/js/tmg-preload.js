53d4b3f5abb795623335a92dccddddffd0ab7eef
topherwhite <topherwhite@gmail.com>
2014-03-27 09:16:23 -0700

    shuffle added

--- public/cdn/js/tmg-preload.js
+++ public/cdn/js/tmg-preload.js
@@ -69,3 +69,16 @@ function randomSort(a,b) {
     var isPosOrNeg = temp>5 ? 1 : -1;
     return( isOddOrEven*isPosOrNeg );
 }
+
+function arrayShuffle(array) {
+    var tmp, current, top = array.length;
+
+    if(top) while(--top) {
+      current = Math.floor(Math.random() * (top + 1));
+      tmp = array[current];
+      array[current] = array[top];
+      array[top] = tmp;
+    }
+
+    return array;
+}
