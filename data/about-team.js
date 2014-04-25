f186eaa830ad2ea2335d2999eb59ec0f8bfcbe37
bryantjcoffey <bryant@totalmediagroup.com>
2014-04-24 18:41:20 -0700

    About Page
    Pics and data, new emails, more people
    
    TOPHER, problems with the layout of them all, I think SG wants 4 across
    and as many down.  I did notice that background image on this page is
    not locking up with the bottom footer, using Safari as well.  Please
    look at.  Thanks!

--- data/about-team.js
+++ data/about-team.js
@@ -4,37 +4,95 @@ var data = [
   [ "MEGAN MCKENNA",
     "PRESIDENT",
     ["MEGAN STUDIED MUSIC FROM FIRST GRADE UP THROUGH HER SOPHOMORE YEAR OF COLLEGE, FULLY EXPECTING TO EVENTUALLY WORK IN THE MUSIC INDUSTRY. BUT WHEN INTRODUCED TO A PROFESSIONAL VIDEO CAMERA BY A FRIEND ON CAMPUS, SHE QUICKLY SWITCHED HER FOCUS TO TECHNICAL JOURNALISM. AFTER LEARNING HOW TO DISCOVER, TELL, AND EDIT A STORY, SHE MOVED TO SAN FRANCISCO TO MAKE IT HER CAREER.","SHE NOW APPLIES HER MANY YEARS OF EXPERIENCE TOWARDS EVERY PROJECT THAT COMES THROUGH THE DOOR.","IN HER SPARE TIME MEGAN CATCHES UP IN MOVIES AND MUSIC, HANGS OUT WITH HER DAUGHTER AND HUSBAND, AND THEIR FUR BABY COOKIE."],
-    "megan@totalmediagroup.com",
+    "megan.mckenna@tmgcreative.tv",
     "megan"
   ],
 
   [ "JACK HSU",
     "CHIEF OPERATING OFFICER",
     ["JACK’s the guy that keeps things running.  He BEGAN FREELANCING FOR TMG AS CHIEF ENGINEER IN THE EARLY ‘90s.  HE REPAIRED AND MAINTAINED ANYTHING FROM MAC AND WINDOWS COMPUTERS TO BROADCAST VTR’S AND ALL TYPES OF VIDEO EDITING AND GRAPHICS SYSTEMS.  HE DESIGNED AND ENGINEERED COMPLETE SYSTEMS.  ","","HE, ALONG WITH ALDO PANATTONI, FOUNDER, WAS AWARDED A PATENT FOR A NEW SYSTEM TO CREATE INSTANT VIDEO DUBS ON LOCATION.  IN 1999, JACK LEFT TMG TO DESIGN AND BUILD A BRAND NEW FACILITY FROM GROUND UP IN TEXAS  BUT JACK REALIZED HE MISSED HIS HOME AT TMG, AND IN 2004 WAS WELCOMED BACK WITH OPEN ARMS.  TMG HAS NEVER LOOKED OR RUN BETTER.  ","","JACK ENJOYS WORKING AROUND THE HOUSE and STUDYING SCRIPTURES.  He travels to Mexico to lend his talents to orphanages, helping with water projects and infrastructure.  ","","Jack’s a BIG FAN OF FIVE GUYS."],
-    "jhsu@totalmediagroup.com",
+    "jack.hsu@tmgcreative.tv",
     "jack"
   ],
 
   [ "STEVEN GREGORY",
     "CREATIVE DIRECTOR",
     ["STEVEN GREGORY (SG) BRINGS AN UNUSUALLY BROAD SET OF CREATIVE SKILLS TO TMG. THROUGHOUT HIS CAREER HE HAS SERVED AS DIRECTOR, PRODUCER, SHOOTER, EDITOR/ANIMATOR, DESIGNED AND BUILT SETS AND WORKED IN MUSIC AND AUDIO POST. HE HAS BEEN AN AWARD WINNING EDITOR CUTTING BOTH LONG FORM DOCUMENTARIES AND TELEVISION SPOTS FOR A WIDE RANGE OF COMMERCIAL CLIENTS.","","Macys - Jc Penny - Applebees - Nestle - Oakland A’s - San Jose Sharks - Boston Bruins - Miami Dolphins - Florida Marlins.","","HE'S AT HOME IN A RECORDING STUDIO, AN EDIT BAY OR A ROOM FULL OF ANIMATORS.","","AN ACCOMPLISHED MUSICIAN AND COMPOSER, STEVEN HAS WORKED ON BOTH SIDES OF THE GLASS WITH A NUMBER OF NOTABLE NATIONAL RECORDING ARTISTS.","","WHETHER IT'S A SIMPLE TECHNICAL EXPLAINER VIDEO OR A HIGH-CONCEPT WORK OF ART, HIS YEARS OF HANDS-ON EXPERIENCE IN SO MANY AREAS OF PRODUCTION GIVE HIM AN INNATE SENSE OF HOW BEST TO CRAFT THE STORY WITH THE HIGHEST DEGREE OF QUALITY WITHIN THE SCOPE OF THE PROJECT.","AN AVID AMATURE HOCKEY PLAYER, STEVEN CAN BE FOUND (WHEN HE'S NOT AT TMG OR SLEEPING), PLAYING AT MANY OF THE BAY AREA HOCKEY RINKS.","","HE SHOOTS...HE SCORES!"],
-    "sg@totalmediagroup.com",
+    "steven.gregory@tmgcreative.tv",
     "sg"
   ],
 
   [ "JEFF CASAGRANDE",
     "MEDIA PRODUCTION MANAGER",
     ["JEFF BRINGS CLOSE TO 20 YEARS OF MEDIA PRODUCTION EXPERIENCE TO TOTAL MEDIA GROUP, HANDLING MUCH OF THE COMPANY’S PROJECT MANAGEMENT NEEDS.","HE WORKS CLOSELY WITH ALL FACILITY AND REMOTE STAFF ON ALL PHASES OF PRODUCTION AND MANAGES A VARIETY OF PROJECTS AND CLIENTS AS WELL.","OFF THE JOB, JEFF LIKES TO SPEND TIME WITH HIS DAUGHTER, TRY NEW RESTAURANTS WITH HIS WIFE, PLAY BLUES HARMONICA, READING, JOGGING AND WATCHING OLD MOVIES."],
-    "jeff@totalmediagroup.com",
+    "jeff.casagrande@tmgcreative.tv",
     "jeff"
   ], 
 
   [ "PHIL SCHULZ",
     "DIRECTOR OF CLIENT SERVICES",
     ["PHIL BRINGS 20 YEARS OF EXPERIENCE TO TMG, HAVING WORKED IN TELEVISION, FEATURE FILM AND CORPORATE VIDEO/MEDIA. BEGINNING HIS CAREER IN PRODUCTION, PHIL SEGUED INTO ACCOUNT MANAGEMENT AND EXECUTIVE PRODUCING WHEN HE LEFT ENTERTAINMENT TO EMPLOY HIS TALENTS IN HELPING CLIENTS TELL THEIR STORY.  WHEN NOT WORKING, PHIL ENJOYS ALL THAT NORTHERN CA HAS TO OFFER WITH HIS WIFE AND THEIR TWO KIDS."],
-    "phil@totalmediagroup.com",
+    "phil.shulz@tmgcreative.tv",
     "phil"
   ],
+  
+  [ "Bryant Coffey",
+    "Facility Technician",
+    ["Bryant is a video editor and motion graphics artist.  Growing up in Lincoln, NE, he moved to Denver, Colorado where he gained his Bachelor's Degree in Visual Effects and Motion Graphics from the Art Institute of Colorado.  Upon completion, he hightailed it to San Francisco and got picked up by TMG Creative.  He’s been loving it ever since.  ","At TMG, Bryant is immersed in a variety of projects ranging from 3D animation and motion graphics to video editorial and website creation.  Additionally, he works closely with operations displaying admirable production and technical abilities with excellence in analyzing and solving problems.  ","In his down time, Bryant enjoys photography, traveling, snowboarding, and many other things pairing him with all the nature Northern California has to offer.  An avid foodie, he also enjoys experiencing the abundance of varied cuisine the Bay Area has to offer.  ","If Bryant has a constant, it is always being on the lookout for exciting possibilities and fun things to do.  "],
+    "bryant.coffey@tmgcreative.tv",
+    "bryant"
+  ],
+  
+  [ "Adam Sturgeon",
+    "Video Editor",
+    ["Adam came to TMG Creative in 2006 after graduating from San Francisco State University with a degree in Radio & Television. After graduation he started out working for the San Francisco Giants video department and then transitioned to freelancing in the live sports broadcasting industry gaining valuable experience and soon after found his opportunity here with TMG.","Starting as a Post-Production Technician, Adam worked his way up the ranks gaining valuable experience and becoming fluent in many of the design, media and editing programs that exist today. Currently Adam is a valuable asset to TMG as he can handle multiple responsibilities ranging from editing to motion graphics to DVD authoring to junior producing.  Adam thoroughly excels working directly with the clients and really enjoys creating something that generates a smile on everyone’s face.  ","When Adam is not working, he enjoys spending time with his wife Cindy and their two sons, Blake and Alex, whether it be at the local park tossing a baseball around or at their annual trips to Disneyland.  Adam is also an avid Bay Area sports fanatic and a proud 49er season ticket holder.   Along with his family they watch or attend as many Giants, 49ers, Warriors and Sharks games as possible.  Adam also tries to squeeze in time polishing his skills out on the golf links or going to concerts to see his favorite bands."],
+    "adam.sturgeon@tmgcreative.tv",
+    "adam"
+  ],
+  
+  [ "Ariella Quatra",
+    "Producer",
+    ["Bryant Coffey is a video editor and a motion graphics designer.  Growing up in Lincoln, NE, he moved to Denver, Colorado where he gained his Bachelor's Degree in Visual Effects and Motion Graphics from the Art Institute of Colorado.  Upon completion of his degree, he hightailed it to San Francisco and got picked up by TMG Creative, where he's been loving it ever since.  ","At TMG he is the jack of all trades.  He is consistently switching between 3D Animation, Video Editing, Website Creation, motion graphics, creating final deliverables per project specifications, helping with technical operations and providing solutions to problems that arise in production, and keeping an eye out for any need arising on the job, and tackling those needs accordingly.  ","In his down time when he's not at work, he enjoys photography, traveling, snowboarding, and a variety of other things that gets him out of the house and into the variety of great nature scenes that Northern California has to offer.  An avid foodie, he also enjoys seeing what the local eateries in the bay area has to offer.  You can generally find him roaming the area, looking for new exciting possibilities and fun things to do.  "],
+    "ariella.quatra@tmgcreative.tv",
+    "ariella"
+  ],
+  
+  [ "Kathy Luo",
+    "Accountant",
+    ["Bryant Coffey is a video editor and a motion graphics designer.  Growing up in Lincoln, NE, he moved to Denver, Colorado where he gained his Bachelor's Degree in Visual Effects and Motion Graphics from the Art Institute of Colorado.  Upon completion of his degree, he hightailed it to San Francisco and got picked up by TMG Creative, where he's been loving it ever since.  ","At TMG he is the jack of all trades.  He is consistently switching between 3D Animation, Video Editing, Website Creation, motion graphics, creating final deliverables per project specifications, helping with technical operations and providing solutions to problems that arise in production, and keeping an eye out for any need arising on the job, and tackling those needs accordingly.  ","In his down time when he's not at work, he enjoys photography, traveling, snowboarding, and a variety of other things that gets him out of the house and into the variety of great nature scenes that Northern California has to offer.  An avid foodie, he also enjoys seeing what the local eateries in the bay area has to offer.  You can generally find him roaming the area, looking for new exciting possibilities and fun things to do.  "],
+    "accounting@tmgcreative.tv",
+    "kathy"
+  ],
+  
+  [ "Ryan Scott",
+    "Motion Graphics Artist",
+    ["Bryant Coffey is a video editor and a motion graphics designer.  Growing up in Lincoln, NE, he moved to Denver, Colorado where he gained his Bachelor's Degree in Visual Effects and Motion Graphics from the Art Institute of Colorado.  Upon completion of his degree, he hightailed it to San Francisco and got picked up by TMG Creative, where he's been loving it ever since.  ","At TMG he is the jack of all trades.  He is consistently switching between 3D Animation, Video Editing, Website Creation, motion graphics, creating final deliverables per project specifications, helping with technical operations and providing solutions to problems that arise in production, and keeping an eye out for any need arising on the job, and tackling those needs accordingly.  ","In his down time when he's not at work, he enjoys photography, traveling, snowboarding, and a variety of other things that gets him out of the house and into the variety of great nature scenes that Northern California has to offer.  An avid foodie, he also enjoys seeing what the local eateries in the bay area has to offer.  You can generally find him roaming the area, looking for new exciting possibilities and fun things to do.  "],
+    "ryan.scott@tmgcreative.tv",
+    "ryan"
+  ],
+  
+  [ "Carol Keys",
+    "Creative Director, Print and Events",
+    ["Bryant Coffey is a video editor and a motion graphics designer.  Growing up in Lincoln, NE, he moved to Denver, Colorado where he gained his Bachelor's Degree in Visual Effects and Motion Graphics from the Art Institute of Colorado.  Upon completion of his degree, he hightailed it to San Francisco and got picked up by TMG Creative, where he's been loving it ever since.  ","At TMG he is the jack of all trades.  He is consistently switching between 3D Animation, Video Editing, Website Creation, motion graphics, creating final deliverables per project specifications, helping with technical operations and providing solutions to problems that arise in production, and keeping an eye out for any need arising on the job, and tackling those needs accordingly.  ","In his down time when he's not at work, he enjoys photography, traveling, snowboarding, and a variety of other things that gets him out of the house and into the variety of great nature scenes that Northern California has to offer.  An avid foodie, he also enjoys seeing what the local eateries in the bay area has to offer.  You can generally find him roaming the area, looking for new exciting possibilities and fun things to do.  "],
+    "carol.keys@tmgcreative.tv",
+    "carol"
+  ],
+  
+  [ "Patrick Roche",
+    "Producer",
+    ["Bryant Coffey is a video editor and a motion graphics designer.  Growing up in Lincoln, NE, he moved to Denver, Colorado where he gained his Bachelor's Degree in Visual Effects and Motion Graphics from the Art Institute of Colorado.  Upon completion of his degree, he hightailed it to San Francisco and got picked up by TMG Creative, where he's been loving it ever since.  ","At TMG he is the jack of all trades.  He is consistently switching between 3D Animation, Video Editing, Website Creation, motion graphics, creating final deliverables per project specifications, helping with technical operations and providing solutions to problems that arise in production, and keeping an eye out for any need arising on the job, and tackling those needs accordingly.  ","In his down time when he's not at work, he enjoys photography, traveling, snowboarding, and a variety of other things that gets him out of the house and into the variety of great nature scenes that Northern California has to offer.  An avid foodie, he also enjoys seeing what the local eateries in the bay area has to offer.  You can generally find him roaming the area, looking for new exciting possibilities and fun things to do.  "],
+    "patrick.roche@tmgcreative.tv",
+    "patrick"
+  ],
+  
+  [ "Jay Koller",
+    "Producer",
+    ["Bryant Coffey is a video editor and a motion graphics designer.  Growing up in Lincoln, NE, he moved to Denver, Colorado where he gained his Bachelor's Degree in Visual Effects and Motion Graphics from the Art Institute of Colorado.  Upon completion of his degree, he hightailed it to San Francisco and got picked up by TMG Creative, where he's been loving it ever since.  ","At TMG he is the jack of all trades.  He is consistently switching between 3D Animation, Video Editing, Website Creation, motion graphics, creating final deliverables per project specifications, helping with technical operations and providing solutions to problems that arise in production, and keeping an eye out for any need arising on the job, and tackling those needs accordingly.  ","In his down time when he's not at work, he enjoys photography, traveling, snowboarding, and a variety of other things that gets him out of the house and into the variety of great nature scenes that Northern California has to offer.  An avid foodie, he also enjoys seeing what the local eateries in the bay area has to offer.  You can generally find him roaming the area, looking for new exciting possibilities and fun things to do.  "],
+    "jay.koller@tmgcreative.tv",
+    "jay"
+  ],
+  
+
 
 
 ["",""]];
