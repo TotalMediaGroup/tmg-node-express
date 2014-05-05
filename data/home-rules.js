4b23cf5143d454b1bc45acd003638ddb32c97c1d
topherwhite <topherwhite@gmail.com>
2014-05-05 15:08:39 -0700

    more rules

--- data/home-rules.js
+++ data/home-rules.js
@@ -3,97 +3,128 @@ var data = [
 
   [ "DO MORE THAN WHAT'S EXPECTED",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_01"
+    "RULE_01",
+    "1"
   ],
 
   [ "TRY NEW THINGS",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_02"
+    "RULE_02",
+    "2"
   ],
 
   [ "TEACH OTHERS WHAT YOU KNOW",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_03"
+    "RULE_03",
+    "3"
   ],
 
   [ "MAKE WORK FUN",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_04"
+    "RULE_04",
+    "4"
   ],
 
   [ "ALWAYS BE CREATING",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_05"
+    "RULE_05",
+    "5"
   ],
 
   [ "MAKE YOUR OWN INSPIRATION",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_06"
+    "RULE_06",
+    "6"
   ],
 
   [ "AVOID THE OBVIOUS",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_07"
+    "RULE_07",
+    "7"
   ],
 
   [ "THE CLIENT IS PART OF THE TEAM",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_08"
+    "RULE_08",
+    "8"
   ],
 
   [ "ACCEPT CREATIVE CRITICISM",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_09"
+    "RULE_09",
+    "9"
   ],
 
   [ "TRUST YOUR TEAMMATES",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_10"
+    "RULE_10",
+    "10"
   ],
 
   [ "WE'RE ALL ROOKIES",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_11"
+    "RULE_11",
+    "11"
   ],
 
   [ "COLLABORATE",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_12"
+    "RULE_12",
+    "12"
   ],
 
   [ "ORGANIZE THE PROJECT",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_13"
+    "RULE_13",
+    "13"
   ],
 
   [ "UNDERSTAND THE SCRIPT",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_14"
+    "RULE_14",
+    "14"
   ],
 
   [ "DON'T CONFUSE WEIRD WITH CREATIVE",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_15"
+    "RULE_15",
+    "15"
   ],
 
   [ "TRASH BAD CONCEPTS",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_16"
+    "RULE_16",
+    "16"
   ],
 
   [ "DON'T WASTE CHI",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_17"
+    "RULE_17",
+    "17"
   ],
 
   [ "TAKE CHANCES",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_18"
+    "RULE_18",
+    "18"
   ],
 
   [ "EMBRACE FOREIGN IDEAS",
     "HOW TO DO REALLY GREAT WORK",
-    "RULE_19"
+    "RULE_19",
+    "19"
+  ],
+
+  [ "PRACTICE",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_21",
+    "21"
+  ],
+
+  [ "DON'T FEAR FAILURE",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_22",
+    "22"
   ],
 
 ["",""]];
