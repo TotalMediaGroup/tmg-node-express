d1bc9e6e6c0865f6e4006f64e9cde1d814621723
topherwhite <topherwhite@gmail.com>
2014-05-06 10:53:38 -0700

    more rules

--- data/home-rules.js
+++ data/home-rules.js
@@ -1,119 +1,119 @@
 
 var data = [
 
-  // [ "DO MORE THAN WHAT'S EXPECTED",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_01",
-  //   "1"
-  // ],
+  [ "DO MORE THAN WHAT'S EXPECTED",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_01",
+    "1"
+  ],
 
-  // [ "TRY NEW THINGS",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_02",
-  //   "2"
-  // ],
+  [ "TRY NEW THINGS",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_02",
+    "2"
+  ],
 
-  // [ "TEACH OTHERS WHAT YOU KNOW",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_03",
-  //   "3"
-  // ],
+  [ "TEACH OTHERS WHAT YOU KNOW",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_03",
+    "3"
+  ],
 
-  // [ "MAKE WORK FUN",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_04",
-  //   "4"
-  // ],
+  [ "MAKE WORK FUN",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_04",
+    "4"
+  ],
 
-  // [ "ALWAYS BE CREATING",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_05",
-  //   "5"
-  // ],
+  [ "ALWAYS BE CREATING",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_05",
+    "5"
+  ],
 
-  // [ "MAKE YOUR OWN INSPIRATION",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_06",
-  //   "6"
-  // ],
+  [ "MAKE YOUR OWN INSPIRATION",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_06",
+    "6"
+  ],
 
-  // [ "AVOID THE OBVIOUS",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_07",
-  //   "7"
-  // ],
+  [ "AVOID THE OBVIOUS",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_07",
+    "7"
+  ],
 
-  // [ "THE CLIENT IS PART OF THE TEAM",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_08",
-  //   "8"
-  // ],
+  [ "THE CLIENT IS PART OF THE TEAM",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_08",
+    "8"
+  ],
 
-  // [ "ACCEPT CREATIVE CRITICISM",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_09",
-  //   "9"
-  // ],
+  [ "ACCEPT CREATIVE CRITICISM",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_09",
+    "9"
+  ],
 
-  // [ "TRUST YOUR TEAMMATES",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_10",
-  //   "10"
-  // ],
+  [ "TRUST YOUR TEAMMATES",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_10",
+    "10"
+  ],
 
-  // [ "WE'RE ALL ROOKIES",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_11",
-  //   "11"
-  // ],
+  [ "WE'RE ALL ROOKIES",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_11",
+    "11"
+  ],
 
-  // [ "COLLABORATE",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_12",
-  //   "12"
-  // ],
+  [ "COLLABORATE",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_12",
+    "12"
+  ],
 
-  // [ "ORGANIZE THE PROJECT",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_13",
-  //   "13"
-  // ],
+  [ "ORGANIZE THE PROJECT",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_13",
+    "13"
+  ],
 
-  // [ "UNDERSTAND THE SCRIPT",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_14",
-  //   "14"
-  // ],
+  [ "UNDERSTAND THE SCRIPT",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_14",
+    "14"
+  ],
 
-  // [ "DON'T CONFUSE WEIRD WITH CREATIVE",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_15",
-  //   "15"
-  // ],
+  [ "DON'T CONFUSE WEIRD WITH CREATIVE",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_15",
+    "15"
+  ],
 
-  // [ "TRASH BAD CONCEPTS",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_16",
-  //   "16"
-  // ],
+  [ "TRASH BAD CONCEPTS",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_16",
+    "16"
+  ],
 
-  // [ "DON'T WASTE CHI",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_17",
-  //   "17"
-  // ],
+  [ "DON'T WASTE CHI",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_17",
+    "17"
+  ],
 
-  // [ "TAKE CHANCES",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_18",
-  //   "18"
-  // ],
+  [ "TAKE CHANCES",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_18",
+    "18"
+  ],
 
-  // [ "EMBRACE FOREIGN IDEAS",
-  //   "HOW TO DO REALLY GREAT WORK",
-  //   "RULE_19",
-  //   "19"
-  // ],
+  [ "EMBRACE FOREIGN IDEAS",
+    "HOW TO DO REALLY GREAT WORK",
+    "RULE_19",
+    "19"
+  ],
 
   [ "PRACTICE",
     "HOW TO DO REALLY GREAT WORK",
